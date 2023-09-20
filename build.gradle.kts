d5e31e06200f83ad48ffb43d5d778a8720a20b21
redevrx <redev.rx@gmail.com>
2023-09-20 16:58:36 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("maven-publish")
 }

ee0386ee6d4f14517849ad5eb09369eea7b0fbe9
redevrx <redev.rx@gmail.com>
2023-09-20 17:02:13 +0700

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://www.jitpack.io" ) }
     }
 }
 dependencyResolutionManagement {
